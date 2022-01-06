3f5d9d35c1c451e6b1e29efea98d5cc87820f963
OhadC <ohadc.me@gmail.com>
2022-01-06 13:16:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-k1vasn
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-k1vasn)
---
+++ src/app/app.module.ts
@@ -0,0 +1,19 @@
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { NgModule } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    DragDropModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
